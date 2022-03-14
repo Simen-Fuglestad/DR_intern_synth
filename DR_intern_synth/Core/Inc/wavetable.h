ede444418b7d6a036e3843b68a0acb08e1034b42
simenfuglestad <sfs@datarespons.no>
2022-03-14 11:18:17 +0100

    fixed stereo channel offset

--- DR_intern_synth/Core/Inc/wavetable.h
+++ DR_intern_synth/Core/Inc/wavetable.h
@@ -9,11 +9,14 @@
 #define INC_WAVETABLE_H_
 
 #include <stdint.h>
-#include "wave_util.h"
+
+typedef enum {
+	SINE, SQUARE, TRIANGLE, SAWTOOTH
+} waveshape_enum;
 
 //Wave gen functions, create a single period T of ns samples
 void wavetable_create(
-		waveshape_t waveshape, uint16_t* out, uint16_t ref_v,
+		waveshape_enum waveshape, uint16_t* out, uint16_t ref_v,
 		uint16_t ns, float amp, uint16_t offset);
 
 void wavetable_create_sine(
@@ -27,12 +30,5 @@ void wavetable_create_triangle(
 
 void wavetable_create_sawtooth(
 		uint16_t* out, uint16_t ref_v, uint16_t ns, float amp, uint16_t offset);
-
-void wavetable_copy(
-		uint16_t* dest, uint32_t* src, uint16_t n);
-
-uint16_t wavetable_compute_sine(uint16_t i, uint16_t ns, uint16_t ref_v);
-
-void filter_phase_shift_fwd(uint16_t* y, uint16_t y_len);
 
 #endif /* INC_WAVETABLE_H_ */
--- DR_intern_synth/Core/Src/wavetable.c
+++ DR_intern_synth/Core/Src/wavetable.c
@@ -16,7 +16,7 @@
 #endif
 
 void wavetable_create(
-		waveshape_t waveshape, uint16_t* out, uint16_t ref_v,
+		waveshape_enum waveshape, uint16_t* out, uint16_t ref_v,
 		uint16_t ns, float amp, uint16_t offset) {
 	memset(out, 0, ns);
 
@@ -41,55 +41,50 @@ void wavetable_create(
 void wavetable_create_sine(
 		uint16_t* out, uint16_t ref_v, uint16_t ns, float amp, uint16_t offset) {
 
-	for (int i = 0; i < ns; i++) {
+	for (int i = 0; i < ns - 1; i++) {
         float sine = sin((i * (2*M_PI)/ns) + 1);
         float scaled = sine * ((ref_v + 1)/2);
 
-        uint16_t s = (uint16_t)(scaled + ref_v/2);
-		out[i] = s * amp;
+        uint16_t s = (uint16_t)(scaled + ref_v/2) * amp;
+		out[i] = s;
+		out[i + 1] = s;
 	}
 }
 
 void wavetable_create_square(
 		uint16_t* out, uint16_t ref_v, uint16_t ns, float amp, uint16_t offset) {
-	for (int i = 0; i < ns/2; i++) {
-		out[i] = ref_v * amp;
+	uint16_t high_val = ref_v * amp;
+	for (int i = 0; i < ns/2; i+=2) {
+		out[i] = high_val;
+		out[i + 1] = high_val;
 	}
 
-	for (int i = ns/2; i < ns; i++) {
+	for (int i = ns/2; i < ns-1; i+=2) {
 		out[i] = 0;
+		out[i + 1] = 0;
 	}
 }
 
 void wavetable_create_triangle(
 		uint16_t* out, uint16_t ref_v, uint16_t ns, float amp, uint16_t offset) {
-	for (int i = 0; i < ns; i++) {
+	for (int i = 0; i < ns - 1; i+=2) {
 		float tri = ((float)2/M_PI) * asin(sin(((2 * M_PI)/ns) * i));
 
 		float scaled = tri * ((ref_v + 1)/2);
 
-		uint16_t s = (uint16_t)(scaled + ref_v/2);
-		out[i] = s * amp;
+		uint16_t s = (uint16_t)(scaled + ref_v/2) * amp;
+		out[i] = s;
+		out[i + 1] = s;
 	}
 }
 
 void wavetable_create_sawtooth(
 		uint16_t* out, uint16_t ref_v, uint16_t ns, float amp, uint16_t offset) {
-	for (int i = 0; i < ns; i++) {		float saw = 2 * ((float)i/ns - floor(0.5 + (float)i/ns));
-		float scaled = saw * ((ref_v + 1)/2) * amp;
-		uint16_t s = (uint16_t)(scaled + ref_v/2);
-		out[i] = s * amp;
+	for (int i = 0; i < ns - 1; i+=2) {
+		float saw = 2 * ((float)i/ns - floor(0.5 + (float)i/ns));
+		float scaled = saw * ((ref_v + 1)/2);
+		uint16_t s = (uint16_t)(scaled + ref_v/2) * amp;
+		out[i] = s;
+		out[i + 1] = s;
 	}
-}
-
-uint16_t wavetable_compute_sine(uint16_t i, uint16_t ns, uint16_t ref_v) {
-    float sine = sin((i * (2*M_PI)/ns) + 1);
-    float scaled = sine * ((ref_v + 1)/2);
-
-    uint16_t s = (uint16_t)(scaled + ref_v/2);
-    return s;
-}
-
-void wavetable_copy(uint16_t* dest, uint32_t* src, uint16_t n) {
-	memcpy(dest, src, n);
 }
